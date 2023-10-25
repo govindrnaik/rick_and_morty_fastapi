90db855aa5fa3283db943ad964c0a21ec712b64c
govind ratnakar naik <govindrnaik.trk@gmail.com>
2023-10-25 21:44:07 +0530

    cards added

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
 import uvicorn
-
+import requests
 
 from fastapi.staticfiles import StaticFiles
 
-app = FastAPI()
+app = FastAPI() 
 
 # Mount the "static" directory as a route to serve static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -20,6 +20,15 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)  
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", context={"request": request})
+
+@app.get("/characters", response_class=HTMLResponse)  
+async def read_root(request: Request):
+
+    response = requests.get("https://rickandmortyapi.com/api/character")
+    data = response.json()
+    print(data["results"])
+
+    return templates.TemplateResponse("index.html", context={"request": request, "results": data["results"]})
 
 
 if __name__ == "__main__":
